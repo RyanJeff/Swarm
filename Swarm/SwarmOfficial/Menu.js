f0b9906e76b9e142979c9e2c66373228035031f1
Andrew-Best <andrew.c.best@gmail.com>
2014-04-29 22:00:13 -0400

    Basic Menu Structure
    I didn't get much done with this, I noted in the file itself that I was
    interrupted. Just updating this so I don't lose the work, albeit
    miniscule, but still some done.

---
+++ Swarm/SwarmOfficial/Menu.js
@@ -0,0 +1,32 @@
+Game.state('Menu', function()
+{
+	//Main menu
+});
+
+Game.state('Hi-Scores', function()
+{
+	//Hi-score table - do we want this as a screen
+	//that is accessible from the main menu?
+});
+
+Game.state('Instructions', function()
+{
+	//Instructions screen
+});
+
+Game.state('Game', function()
+{
+	//Game screen
+});
+
+Game.state('Loading', function()
+{
+	//Loading Screen
+	//We can also load all of our assets here
+	
+});
+
+/*
+Incomplete right now (obviously), I've been interrupted before
+I could get much of real value done
+*/
